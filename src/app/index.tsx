fb437f54bd1401eca0d1ae5cd1a2a5e3dbcf548a
im-na0 <myblu1008@gmail.com>
2024-11-15 17:31:49 +0900

    feat: Pretendard 웹폰트 추가

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,11 @@
-import AppRouter from "./providers/AppRouter";
-import QueryProvider from "./providers/QueryProvider";
-import "./styles/main.css";
+import AppRouter from './providers/AppRouter';
+import QueryProvider from './providers/QueryProvider';
+import dayjs from 'dayjs';
+import './styles/globals.css';
+import './styles/main.css';
+
+import 'dayjs/locale/ko';
+dayjs.locale('ko');
 
 function App() {
   return (

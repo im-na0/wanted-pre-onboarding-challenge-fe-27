a2bbfb33849c69f5a2be5d6e44297a0356e2e423
im-na0 <myblu1008@gmail.com>
2024-11-15 17:34:03 +0900

    feat: shared ui 헤더 컴포넌트 추가

---
+++ src/shared/ui/header.tsx
@@ -0,0 +1,30 @@
+import dayjs from 'dayjs';
+import { Button } from './button';
+import { LogOut } from 'lucide-react';
+
+export function Header() {
+  const today = dayjs().format('MM월 DD일');
+
+  return (
+    <div className="border-b border-neutral-100 bg-white bg-opacity-75 p-4">
+      <div className="flex items-center">
+        {/* 헤더 텍스트 */}
+        <div className="flex-1">
+          <div className="text-base font-bold tracking-tight text-neutral-800">
+            TodoList.
+          </div>
+          <div className="text-xl font-bold leading-tight tracking-tight text-neutral-900">
+            {today}
+          </div>
+        </div>
+
+        {/* 로그아웃 버튼 */}
+        <div className="flex justify-end">
+          <Button variant="outline" size="icon">
+            <LogOut />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+}
